68d701fbe33f7536dab42eca2b4026949f867e01
MorreG <65241109+MorreG@users.noreply.github.com>
2025-03-27 13:29:33 +0100

    Update button.js

--- JS/button.js
+++ JS/button.js
@@ -1,6 +1,7 @@
 
 export function button() {
     const buttons = document.querySelectorAll('.read-more-btn');
+    const submitButton = document.querySelector('.submit-button')
     
     buttons.forEach(btn => {
         btn.addEventListener('mousemove', (e) => {
@@ -11,6 +12,15 @@ export function button() {
             btn.style.setProperty('--x', x + 'px');
             btn.style.setProperty('--y', y + 'px');
         });
+    });
+
+    submitButton.addEventListener('mousemove', (e) => {
+        const rect = btn.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        
+        btn.style.setProperty('--x', x + 'px');
+        btn.style.setProperty('--y', y + 'px');
     });
 }
 
