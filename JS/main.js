c2ca70c88cc4f82bb4e1c126f2464c6d013b65df
MorreG <65241109+MorreG@users.noreply.github.com>
2025-03-27 14:53:06 +0100

    Smooth scroll & more
    Blandat uppdateringar + ny bild för about.

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,8 @@
 import { header } from "../JS/header.js";
 import { button } from "../JS/button.js";
+
+document.querySelector('a[href="../Sidor/About.html#Contact"]').addEventListener('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('scrollToContact', 'true'); // Spara att vi ska scrolla efter sidbyte
+    window.location.href = "../Sidor/About.html#Contact"; // Navigera till About.html
+  });
