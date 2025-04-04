e43d4229a54a0ef71d8ab45cf7aaaa0d9297698d
MorreG <65241109+MorreG@users.noreply.github.com>
2025-04-04 14:09:15 +0200

    New JS integration

--- JS/button.js
+++ JS/button.js
@@ -1,27 +1,22 @@
+const buttons = document.querySelectorAll('.read-more-btn');
+const submitButton = document.querySelector('.submit-button')
 
-export function button() {
-    const buttons = document.querySelectorAll('.read-more-btn');
-    const submitButton = document.querySelector('.submit-button')
-    
-    buttons.forEach(btn => {
-        btn.addEventListener('mousemove', (e) => {
-            const rect = btn.getBoundingClientRect();
-            const x = e.clientX - rect.left;
-            const y = e.clientY - rect.top;
-            
-            btn.style.setProperty('--x', x + 'px');
-            btn.style.setProperty('--y', y + 'px');
-        });
-    });
-
-    submitButton.addEventListener('mousemove', (e) => {
-        const rect = submitButton.getBoundingClientRect();
+buttons.forEach(btn => {
+    btn.addEventListener('mousemove', (e) => {
+        const rect = btn.getBoundingClientRect();
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
         
-        submitButton.style.setProperty('--x', x + 'px');
-        submitButton.style.setProperty('--y', y + 'px');
+        btn.style.setProperty('--x', x + 'px');
+        btn.style.setProperty('--y', y + 'px');
     });
-}
+});
 
-button();
+submitButton.addEventListener('mousemove', (e) => {
+    const rect = submitButton.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    
+    submitButton.style.setProperty('--x', x + 'px');
+    submitButton.style.setProperty('--y', y + 'px');
+});
