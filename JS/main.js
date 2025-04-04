77d76bd8e3d39408a7c43293e835ab12504967a2
MorreG <65241109+MorreG@users.noreply.github.com>
2025-04-04 15:45:38 +0200

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -33,12 +33,3 @@ window.addEventListener('scroll', () => {
         lastScroll = currentScroll;
     }
 });
-
-
-//Nedan är tillfälligt 
-
-document.querySelector('a[href="../Sidor/About.html#Contact"]').addEventListener('click', function(e) {
-    e.preventDefault();
-    sessionStorage.setItem('scrollToContact', 'true'); // Spara att vi ska scrolla efter sidbyte
-    window.location.href = "../Sidor/About.html#Contact"; // Navigera till About.html
-  });
