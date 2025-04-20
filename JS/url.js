576010138cfdf39efbb442282a7cdd67b748fb36
MorreG <65241109+MorreG@users.noreply.github.com>
2025-04-20 13:04:01 +0200

    URL script file
    STWK felstavning

---
+++ JS/url.js
@@ -0,0 +1,33 @@
+// Get the current page filename
+const currentPage = window.location.pathname.split('/').pop();
+
+// Select all navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+anchor.addEventListener('click', function(e) {
+    // Parse the href into its components
+    const href = this.getAttribute('href');
+    const hrefParts = href.split('#');
+    const targetPage = hrefParts[0] || currentPage;
+    const targetSection = hrefParts[1];
+    
+    // If this is a link to a section on the current page
+    if ((targetPage === currentPage || targetPage === '') && targetSection) {
+    // Prevent default behavior
+    e.preventDefault();
+    
+    // Find and scroll to the target element
+    const targetElement = document.getElementById(targetSection);
+    if (targetElement) {
+        targetElement.scrollIntoView({
+        behavior: 'smooth'
+        });
+        
+        // Remove the fragment from URL after scrolling
+        setTimeout(() => {
+        window.history.replaceState({}, document.title, window.location.pathname);
+        }, 10);
+    }
+    }
+    // For links to other pages, let the default behavior handle it
+});
+});
